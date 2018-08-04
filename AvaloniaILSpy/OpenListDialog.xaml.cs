c00af9b6dc110eb377b6e2211b173452e8ee3432
Jeffrey Ye <lightning.x.revenant@gmail.com>
2018-08-04 10:04:13 -0700

    Fix open list

--- AvaloniaILSpy/OpenListDialog.xaml.cs
+++ AvaloniaILSpy/OpenListDialog.xaml.cs
@@ -34,34 +34,46 @@ namespace AvaloniaILSpy
 	public partial class OpenListDialog : DialogWindow
 	{
 
-		public const string DotNet4List = ".NET 4 (WPF)";
-		public const string DotNet35List = ".NET 3.5";
-		public const string ASPDotNetMVC3List = "ASP.NET (MVC3)";
+		public const string DotNetCoreList = ".NET Core";
 
 		readonly AssemblyListManager manager;
 
 		internal ListBox listView;
-		internal Button okButton;
-		internal Button deleteButton;
+        internal Button okButton;
+        internal Button cancelButton;
+        internal Button deleteButton;
+        internal Button createButton;
+        internal Button resetButton;
 
 		public OpenListDialog()
-		{
+        {
+            manager = MainWindow.Instance.assemblyListManager;
 			InitializeComponent();
 #if DEBUG
 			this.AttachDevTools();
 #endif
-			manager = MainWindow.Instance.assemblyListManager;
 		}
 
 		private void InitializeComponent()
 		{
 			AvaloniaXamlLoader.Load(this);
 			listView = this.FindControl<ListBox>("listView");
-			okButton = this.FindControl<Button>("okButton");
-			deleteButton = this.FindControl<Button>("deleteButton");
+            okButton = this.FindControl<Button>("okButton");
+            cancelButton = this.FindControl<Button>("cancelButton");
+            deleteButton = this.FindControl<Button>("deleteButton");
+            createButton = this.FindControl<Button>("createButton");
+            resetButton = this.FindControl<Button>("resetButton");
+
+            listView.SelectionChanged += ListView_SelectionChanged;
+            listView_Loaded(this, EventArgs.Empty);
+            listView.DoubleTapped += listView_MouseDoubleClick;
+            okButton.Click += OKButton_Click;
+            deleteButton.Click += DeleteButton_Click;
+            createButton.Click += CreateButton_Click;
+            resetButton.Click += ResetButton_Click;
 		}
 
-		private void listView_Loaded(object sender, RoutedEventArgs e)
+        private void listView_Loaded(object sender, EventArgs e)
 		{
 			listView.Items = manager.AssemblyLists;
 			CreateDefaultAssemblyLists();
@@ -77,7 +89,12 @@ namespace AvaloniaILSpy
 		{
 			//this.DialogResult = true;
 			Close(true);
-		}
+        }
+
+        void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close(false);
+        }
 
 		public string SelectedListName
 		{
@@ -89,87 +106,22 @@ namespace AvaloniaILSpy
 
 		private void CreateDefaultAssemblyLists()
 		{
-			if (!manager.AssemblyLists.Contains(DotNet4List))
+            if (!manager.AssemblyLists.Contains(DotNetCoreList))
 			{
-				AssemblyList dotnet4 = new AssemblyList(DotNet4List);
-				AddToList(dotnet4, "mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System.Data.DataSetExtensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System.Xaml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "System.Xml.Linq, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet4, "Microsoft.CSharp, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-				AddToList(dotnet4, "PresentationCore, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(dotnet4, "PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(dotnet4, "WindowsBase, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-
-				if (dotnet4.assemblies.Count > 0)
+                AssemblyList netcore = new AssemblyList(DotNetCoreList);
+                //AddToList(netcore, "System.Private.CoreLib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=7cec85d7bea7798e");
+                AddToList(netcore, "netstandard.library");
+				if (netcore.assemblies.Count > 0)
 				{
-					manager.CreateList(dotnet4);
-				}
-			}
-
-			if (!manager.AssemblyLists.Contains(DotNet35List))
-			{
-				AssemblyList dotnet35 = new AssemblyList(DotNet35List);
-				AddToList(dotnet35, "mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "System.Core, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "System.Data, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "System.Data.DataSetExtensions, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "System.Xml, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "System.Xml.Linq, Version=3.5.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(dotnet35, "PresentationCore, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(dotnet35, "PresentationFramework, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(dotnet35, "WindowsBase, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-
-				if (dotnet35.assemblies.Count > 0)
-				{
-					manager.CreateList(dotnet35);
-				}
-			}
-
-			if (!manager.AssemblyLists.Contains(ASPDotNetMVC3List))
-			{
-				AssemblyList mvc = new AssemblyList(ASPDotNetMVC3List);
-				AddToList(mvc, "mscorlib, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.ComponentModel.DataAnnotations, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Configuration, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-				AddToList(mvc, "System.Core, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.Data, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.Data.DataSetExtensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.Data.Entity, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-				AddToList(mvc, "System.EnterpriseServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-				AddToList(mvc, "System.Web, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-				AddToList(mvc, "System.Web.Abstractions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.ApplicationServices, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.DynamicData, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.Entity, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.Web.Extensions, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.Mvc, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.Routing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.Services, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-				AddToList(mvc, "System.Web.WebPages, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Web.Helpers, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
-				AddToList(mvc, "System.Xml, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "System.Xml.Linq, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089");
-				AddToList(mvc, "Microsoft.CSharp, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a");
-
-				if (mvc.assemblies.Count > 0)
-				{
-					manager.CreateList(mvc);
+					manager.CreateList(netcore);
 				}
 			}
 		}
 
-		private void AddToList(AssemblyList list, string FullName)
+		private void AddToList(AssemblyList list, string name)
 		{
-			AssemblyNameReference reference = AssemblyNameReference.Parse(FullName);
-			string file = GacInterop.FindAssemblyInNetGac(reference);
+			//AssemblyNameReference reference = AssemblyNameReference.Parse(FullName);
+            string file = typeof(string).Assembly.Location;
 			if (file != null)
 				list.OpenAssembly(file);
 		}
@@ -214,9 +166,9 @@ namespace AvaloniaILSpy
 			CreateDefaultAssemblyLists();
 		}
 
-		private void listView_MouseDoubleClick(object sender, PointerPressedEventArgs e)
+        private void listView_MouseDoubleClick(object sender, RoutedEventArgs e)
 		{
-			if (e.MouseButton == MouseButton.Left && e.ClickCount >= 2 && listView.SelectedItem != null)
+			if (listView.SelectedItem != null)
 				//this.DialogResult = true;
 				this.Close(true);
 		}
