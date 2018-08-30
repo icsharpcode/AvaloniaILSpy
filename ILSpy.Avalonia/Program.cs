f4dd21588b073cff39e43f54da8788a49a1c5162
Jeffrey Ye <lightning.x.revenant@gmail.com>
2018-08-30 10:47:54 -0700

    Add missing project

---
+++ ILSpy.Avalonia/Program.cs
@@ -0,0 +1,28 @@
+﻿using Avalonia;
+using System.IO;
+using System.Reflection;
+using Avalonia.Logging.Serilog;
+
+namespace ICSharpCode.ILSpy
+{
+    static class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+            BuildAvaloniaApp().Start<MainWindow>();
+        }
+
+        /// <summary>
+        /// This method is needed for IDE previewer infrastructure
+        /// </summary>
+        public static AppBuilder BuildAvaloniaApp()
+                                          => AppBuilder.Configure<App>()
+#if DEBUG
+                                            .LogToDebug()
+#endif
+                                            .UsePlatformDetect();
+    }
+}
