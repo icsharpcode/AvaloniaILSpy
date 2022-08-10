2c6a3a16addb14406fc64a99c70ea7d655f291ef
Aaron Robinson <aaronrobin1234@gmail.com>
2022-08-09 19:31:48 -0500

    Update to ILSpy 8.0 Preview 2

--- ILSpy.Core/Properties/AssemblyInfo.cs
+++ ILSpy.Core/Properties/AssemblyInfo.cs
@@ -35,11 +35,11 @@ using System.Diagnostics.CodeAnalysis;
 
 internal static class RevisionClass
 {
-	public const string Major = "7";
-	public const string Minor = "2";
+	public const string Major = "8";
+	public const string Minor = "0";
 	public const string Build = "0";
 	public const string Revision = "0";
-	public const string VersionName = "rc";
+	public const string VersionName = "preview2";
 	
 	public const string FullVersion = Major + "." + Minor + "." + Build + "." + Revision + "." + VersionName;
 }
